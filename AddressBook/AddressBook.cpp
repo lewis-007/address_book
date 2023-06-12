e5877dfab670385aa984328bc164fab7da7d0aaa
liusai <ls973383360@163.com>
2023-06-12 10:17:27 +0800

    commit address book code

--- AddressBook/AddressBook.cpp
+++ AddressBook/AddressBook.cpp
@@ -1,20 +1,296 @@
-﻿// AddressBook.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
+#include <string>
+using namespace std;
+#define MAX 1000        //宏定义
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//设计联系人结构体
+struct Person {
+	//姓名
+	string m_Name;
+	//性别
+	int m_Sex;
+	//年龄
+	int m_Age;
+	//电话
+	string m_Phone;
+	//住址
+	string m_Addr;
+};
+
+//设计通讯录结构体
+struct Addressbooks {
+	//通讯录中保存的联系人数组
+	struct Person personArray[MAX];
+	//通讯录中当前保存联系人个数
+	int m_Size;
+};
+
+//添加联系人
+void addPerson(Addressbooks * abs) {
+	//判断通讯录是否已满
+	if (abs->m_Size == MAX) {
+		std::cout << "通讯录已满，无法添加!" << endl;
+		return;
+	}
+	else {
+		//添加联系人
+		//姓名
+		string name;
+		cout << "请输入姓名：" << endl;
+		cin >> name;
+		abs->personArray[abs->m_Size].m_Name = name;
+		//性别
+		cout << "1 --- 男" << endl;
+		cout << "2 --- 女" << endl;
+		while (true)
+		{
+			cout << "请输入性别：" << endl;
+			int sex = 0;
+			cin >> sex;
+			if (sex == 1 || sex == 2) {
+				abs->personArray[abs->m_Size].m_Sex = sex;
+				break;
+			}
+			else {
+				cout << "您输入的有误，请重新输入！" << endl;
+			}
+		}
+		//年龄
+		cout << "请输入年龄：" << endl;
+		int age;
+		cin >> age;
+		abs->personArray[abs->m_Size].m_Age = age;
+		//电话
+		cout << "请输入电话：" << endl;
+		string phone;
+		cin >> phone;
+		abs->personArray[abs->m_Size].m_Phone = phone;
+		//住址
+		cout << "请输入家庭住址：" << endl;
+		string addr;
+		cin >> addr;
+		abs->personArray[abs->m_Size].m_Addr = addr;
+
+		//更新通讯录人数
+		abs->m_Size++;
+
+		system("pause");  //按任意键继续
+		system("cls");    //清屏
+	}
+};
+
+//显示联系人
+void showPerson(Addressbooks * abs) {
+	if (abs->m_Size == 0) {
+		cout << "当前通讯录为空" << endl;
+		return;
+	}
+	else {
+		for (int i = 0; i < abs->m_Size; i++) {
+
+			cout << "姓名： " << abs->personArray[i].m_Name << "\t";
+			cout << "性别： " << (abs->personArray[i].m_Sex == 1 ? "男" : "女") << "\t";
+			cout << "年龄： " << abs->personArray[i].m_Age << "\t";
+			cout << "电话： " << abs->personArray[i].m_Phone << "\t";
+			cout << "住址： " << abs->personArray[i].m_Addr << endl;
+		}
+	}
+
+	system("pause");
+	system("cls");
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+//查找联系人是否存在,存在返回下标，不存在返回-1
+int isExist(Addressbooks * abs, string name) {
+	for (int i = 0; i < abs->m_Size; i++) {
+		if (abs->personArray[i].m_Name == name) {
+			return i;
+		}
+	}
+	return -1;  //如果遍历结束都没有找到，返回 -1
+}
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+//删除联系人
+void deletePerson(Addressbooks * abs) {
+	cout << "请输入删除联系人姓名： ";
+	string name;
+	cin >> name;
+
+	int ret = isExist(abs, name);
+
+	if (ret != -1) {
+		for (int i = ret; i < abs->m_Size; i++) {
+			abs->personArray[i] = abs->personArray[i + 1];
+		}
+		abs->m_Size--;   //更新通讯录中的人数
+		cout << "删除成功" << endl;
+	}
+	else {
+		cout << "找到此人" << endl;
+	}
+
+	system("pause");
+	system("cls");
+};
+
+//查找联系人
+void findPerson(Addressbooks * abs) {
+	cout << "请输入要查找的联系人： ";
+	string name;
+	cin >> name;
+
+	int ret = isExist(abs, name);
+	if (ret != -1) {
+		Person person = abs->personArray[ret];
+		cout << "姓名： " << person.m_Name << "\t";
+		cout << "性别： " << person.m_Sex << "\t";
+		cout << "年龄： " << person.m_Age << "\t";
+		cout << "电话： " << person.m_Phone << "\t";
+		cout << "住址： " << person.m_Addr << endl;
+	}
+	else
+	{
+		cout << "查无此人" << endl;
+	}
+
+	system("pause");
+	system("cls");
+}
+
+//修改联系人
+void modifyPerson(Addressbooks * abs) {
+	cout << "请输入您要修改的联系人： " << endl;
+	string name;
+	cin >> name;
+
+	int ret = isExist(abs, name);
+
+	if (ret != -1) {
+		Person * person = &abs->personArray[ret];
+		//姓名
+		cout << "请输入姓名： " << endl;
+		string name;
+		cin >> name;
+		person->m_Name = name;
+
+		//性别
+		cout << "请输入性别： " << endl;
+		cout << "1 --- 男" << endl;
+		cout << "2 --- 女" << endl;
+		int sex;
+		while (true)
+		{
+			cin >> sex;
+			if (sex == 1 || sex == 2) {
+				person->m_Sex = sex;
+				break;
+			}
+			else {
+				cout << "输入有误，请重新输入！" << endl;
+			}
+		}
+
+		//年龄
+		cout << "亲输入年龄： " << endl;
+		int age;
+		cin >> age;
+		person->m_Age = age;
+
+		//联系电话
+		cout << "请输入联系电话： " << endl;
+		string phone;
+		cin >> phone;
+		person->m_Phone = phone;
+
+		//家庭住址
+		cout << "请输入家庭住址： " << endl;
+		string address;
+		cin >> address;
+		person->m_Addr = address;
+	}
+	else {
+		cout << "查无此人" << endl;
+	}
+
+	system("pause");
+	system("cls");
+}
+
+//清空联系人
+void cleanPerson(Addressbooks * abs) {
+	cout << "确定要清空通讯录吗？y/n" << endl;
+	char flag;
+	cin >> flag;
+	if (flag == 'y') {
+		abs->m_Size = 0;
+		cout << "通讯录已清空" << endl;
+	}
+	else {
+	}
+
+	system("pause");
+	system("cls");
+}
+
+//显示主菜单
+void showMenu() {
+	cout << "***********************************" << endl;
+	cout << "********** 1.添加联系人 ***********" << endl;
+	cout << "********** 2.显示联系人 ***********" << endl;
+	cout << "********** 3.删除联系人 ***********" << endl;
+	cout << "********** 4.查找联系人 ***********" << endl;
+	cout << "********** 5.修改联系人 ***********" << endl;
+	cout << "********** 6.清空联系人 ***********" << endl;
+	cout << "********** 0.退出通讯录 ***********" << endl;
+	cout << "***********************************" << endl;
+}
+
+int main() {
+
+	int select = 0; //创建用户选择输入的变量
+
+	//创建通讯录结构体变量
+	struct Addressbooks abs;
+	//初始化通讯录当前人员个数
+	abs.m_Size = 0;
+
+	while (true)
+	{
+		//菜单调用
+		showMenu();
+		cin >> select;
+
+		switch (select) {
+		case 1:    //添加联系人
+			addPerson(&abs);
+			break;
+		case 2:    //显示联系人
+			showPerson(&abs);
+			break;
+		case 3:    //删除联系人
+			deletePerson(&abs);
+			break;
+		case 4:    //查找联系人
+			findPerson(&abs);
+			break;
+		case 5:    //修改联系人
+			modifyPerson(&abs);
+			break;
+		case 6:    //清空联系人
+			cleanPerson(&abs);
+			break;
+		case 0:    //退出通讯录
+			cout << "欢迎下次使用" << endl;
+			system("pause");
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
+
+	system("pause");
+	system("cls");
+	return 0;
+}
